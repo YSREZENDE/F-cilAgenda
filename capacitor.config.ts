0db9c824fa301fbf798083e8aee7de5487351b00
Ys <107312818+YSREZENDE@users.noreply.github.com>
2024-12-05 19:40:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FacilAgenda',
+  webDir: 'www'
+};
+
+export default config;
